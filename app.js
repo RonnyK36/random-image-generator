b6488b8201e67232ad378ef14194b12845de51da
RonnyK36 <ronnykelvyne3@gmail.com>
2022-08-17 18:02:36 +0300

    Completed JS to fetch random images and use button to load more random images

--- app.js
+++ app.js
@@ -1,4 +1,16 @@
 const imageContainerEl = document.querySelector(".image-container");
 const btnEl = document.querySelector(".btn");
 
-btnEl.addEventListener("click", () => { console.log("Clicked"); });
+btnEl.addEventListener("click", () => {
+
+    addNewImages();
+});
+
+function addNewImages() {
+    let imageNumber = 10;
+    for (let index = 0; index < imageNumber; index++) {
+        const newIgmEl = document.createElement("img");
+        newIgmEl.src = `https://picsum.photos/700?random=${Math.ceil(Math.random() * 2000)}`;
+        imageContainerEl.appendChild(newIgmEl);
+    }
+}
